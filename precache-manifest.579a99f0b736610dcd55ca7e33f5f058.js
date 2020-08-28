4c953ec3c13fb6c8f3bff9a61375435b451a436c
Phu Phan <phu.phan@saigontechnology.com>
2020-08-28 13:09:40 +0700

    Updates

---
+++ precache-manifest.579a99f0b736610dcd55ca7e33f5f058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e811b26d302fcdc654e8137cf55ac1d1",
+    "url": "/github-demo/index.html"
+  },
+  {
+    "revision": "052032cc0722ec077883",
+    "url": "/github-demo/static/css/2.61d06a77.chunk.css"
+  },
+  {
+    "revision": "e44fe43d2d52428f51d7",
+    "url": "/github-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "052032cc0722ec077883",
+    "url": "/github-demo/static/js/2.6e84a6bf.chunk.js"
+  },
+  {
+    "revision": "0f87e0cfbb145332e9889452e79883f4",
+    "url": "/github-demo/static/js/2.6e84a6bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44fe43d2d52428f51d7",
+    "url": "/github-demo/static/js/main.db0d435b.chunk.js"
+  },
+  {
+    "revision": "b30fb56441f5230ef072",
+    "url": "/github-demo/static/js/runtime-main.76605240.js"
+  }
+]);
